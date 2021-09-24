dd63d132b765629cc170eb772736e9ace10c887b
Pascal Parois <pascal22p@parois.net>
2021-09-24 15:32:37 +0000

    fixed bugs

--- Owntracks/insertLocation.php
+++ Owntracks/insertLocation.php
@@ -1,7 +1,9 @@
 <?php
-error_reporting(E_ALL);
+error_reporting(0);
+ini_set("log_errors", 1);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$sql = "INSERT IGNORE INTO locations (acc, alt, lat, lon, tid, tst, vac, vel, p) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO locations (acc, alt, lat, lon, tid, tst, vac, vel, p, user) VALUES (?, ?, ?, ?, ?, FROM_UNIXTIME(?), ?, ?, ?, ?)";
 
 $location = json_decode(file_get_contents('php://input'));
 
@@ -11,13 +13,32 @@ $database = $_GET['database'];
 
 $conn = new mysqli('localhost', $user, $password, $database);
 if ($conn->connect_error) {
+  error_log("Connection failed: " . $conn->connect_error);
   die("Connection failed: " . $conn->connect_error);
 }
 
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("iiddsiiid", $location->acc, $location->alt, $location->lat, $location->lon, $location->tid, $location->tst, $location->vac, $location->vel, $location->p);
-$result = $stmt->execute();
-print($result);
+try {
+  $stmt = $conn->prepare($sql);
+  if(property_exists($location, 'vel')) {
+    $vel = $location->vel;
+  } else {
+    $vel = 0;
+  }
+  $stmt->bind_param("iiddsiiids", $location->acc, $location->alt, $location->lat, $location->lon, $location->tid, $location->tst, $location->vac, $vel, $location->p, $location->user);
+  $result = $stmt->execute();
+  echo "Success";
+  http_response_code(200);
+} catch (Exception $e) {
+    if ($stmt->errno == 1062) {
+      echo "duplicated";
+      http_response_code(422);
+    } else {
+      error_log('Caught exception: ('.strval($stmt->errno).') '.  $e->getMessage());
+      echo 'Caught exception: '.  $e->getMessage();
+      http_response_code(500);
+      exit();
+   }
+}
 $stmt->close();
 $conn->close();
 
--- Owntracks/insertSteps.php
+++ Owntracks/insertSteps.php
@@ -1,14 +1,16 @@
 <?php
-error_reporting(E_ALL);
+error_reporting(0);
+ini_set("log_errors", 1);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-function checkOverlaps(conn, from, to) {
+function checkOverlaps($conn, $from, $to) {
   $sql = "SELECT COUNT(*) AS cpt FROM steps WHERE ?>fromdate AND ?<toDate OR ?>fromdate AND ?<toDate";
-  $stmt->bind_param("ii", from, from, to, to);
+  $stmt->bind_param("iiii", $from, $from, $to, $to);
   $result = $stmt->execute();
-  return $result->fetch_object()->cpt > 0
+  return $result->fetch_object()->cpt > 0;
 }
 
-$sql = "INSERT INTO steps (fromDate, toDate, steps, distance, floorsup, floorsdown) VALUES (?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO steps (fromDate, toDate, steps, distance, floorsup, floorsdown, user) VALUES (FROM_UNIXTIME(?), FROM_UNIXTIME(?), ?, ?, ?, ?, ?)";
 
 $steps = json_decode(file_get_contents('php://input'));
 
@@ -21,9 +23,23 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("iiiiii", $steps->from, $steps->to, $steps->steps, $steps->distance, $steps->floorsup, $steps->floorsdown);
-$result = $stmt->execute();
+try {
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("iiiiiis", $steps->from, $steps->to, $steps->steps, $steps->distance, $steps->floorsup, $steps->floorsdown, $steps->user);
+  $result = $stmt->execute();
+  echo "Success";
+  http_response_code(200);
+} catch (Exception $e) {
+    if ($stmt->errno == 1062) {
+      echo "duplicated";
+      http_response_code(422);
+    } else {
+      error_log('Caught exception: ('.strval($stmt->errno).') '.  $e->getMessage());
+      echo 'Caught exception: '.  $e->getMessage();
+      http_response_code(500);
+   }
+}
+
 $stmt->close();
 $conn->close();
 
